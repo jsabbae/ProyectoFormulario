a7548726e53c771f3328b6f28c73ae07c50f8419
juanma785 <jsabbae2702@g.educaand.es>
2024-05-13 00:25:12 +0200

    cambios

---
+++ src/main/java/com/es/proyectoFormulario/ui/panels/PanelModificacion.java
@@ -0,0 +1,13 @@
+package main.java.com.es.proyectoFormulario.ui.panels;
+
+import main.java.com.es.proyectoFormulario.ui.frames.FrameLogin;
+
+import javax.swing.*;
+
+public class PanelModificacion extends JPanel {
+    private FrameLogin framePadre;
+
+    public PanelModificacion(FrameLogin framePadre) {
+        this.framePadre = framePadre;
+    }
+}
